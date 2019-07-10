415f888cb06fd42ee318896121560f1ba9f19fc4
Mofiz Uddin <mofizuddin@live.co.uk>
2019-07-10 07:56:24 +0000

    fixed routes

---
+++ app.js
@@ -0,0 +1,25 @@
+var express 		= require("express"),
+ 	app 			= express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.get("/",(req,res)=>{
+	res.render("index");
+});
+app.get("/patatap",(req,res)=>{
+	res.render("patatapClone/index");
+});
+app.get("/todo",(req,res)=>{
+	res.render("todolistProject/index");
+});
+app.get("/color-game",(req,res)=>{
+	res.render("colorGame/colorGame");
+});
+app.get("/photo-grid",(req,res)=>{
+	res.render("photo-grid/photogrid");
+});
+app.get("/candy",(req,res)=>{
+	res.render("Bootstrap/Bootstrap4/candy/index");
+});
+app.listen(process.env.PORT,process.env.IP,()=>{
+	console.log("server started");
+});
